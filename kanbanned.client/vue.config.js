d8303730d2276ed18eb1d3ddcb123cde613178f7
Kody Dudley <kodymdudley@gmail.com>
2020-11-13 11:20:16 -0700

    deploy with fix

--- kanbanned.client/vue.config.js
+++ kanbanned.client/vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
   outputDir: '../kanbanned.server/client',
-  productionSourceMap: false
+  productionSourceMap: false,
+  chainWebpack: config=>{
+    config.plugins
+    .delete("split-manifest")
+    .delete("inline-manifest")
+  }
 }
