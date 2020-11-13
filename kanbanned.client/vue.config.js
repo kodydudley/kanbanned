deb7f76d99f3d7d89b584710fd519c4edc207502
Kody Dudley <kodymdudley@gmail.com>
2020-11-13 11:45:55 -0700

    removed meta tag

--- kanbanned.client/vue.config.js
+++ kanbanned.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/',
+  // publicPath: '/',
   outputDir: '../kanbanned.server/client',
   productionSourceMap: false,
   // chainWebpack: config=>{
