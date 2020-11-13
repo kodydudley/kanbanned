0bd1218a50239d32c73f7c81bf78e2c5e0f97b00
Kody Dudley <kodymdudley@gmail.com>
2020-11-13 15:03:12 -0700

    edtis

--- kanbanned.server/server/Startup.js
+++ kanbanned.server/server/Startup.js
@@ -24,7 +24,7 @@ export default class Startup {
           directives: {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", `https://${domain}/*`, '*'],
-            styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
+            styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'", '*'],
             imgSrc: ["'self'", '*.githubusercontent.com', '*.blob.core.windows.net', '*'],
             connectSrc: ["'self'", `https://${domain}/oauth/token`, `https://${domain}/userinfo`],
             fontSrc: ["'self'", 'https://fonts.gstatic.com'],
